b2a2ea83b6bfcaa19be1b4fe31a3f6d281695fa6
CryptJS13 <cryptjs13@protonmail.com>
2025-08-29 11:15:16 +0200

    Fix type error

--- src/types/globalAppTypes.ts
+++ src/types/globalAppTypes.ts
@@ -291,13 +291,7 @@ export interface VaultContextData {
 declare global {
   // Minimal typing for the injected provider
   interface Window {
-    ethereum?: {
-      request: (args: { method: string; params?: unknown[] | object }) => Promise<unknown>;
-      on: (event: string, listener: (...args: unknown[]) => void) => void;
-      removeListener: (event: string, listener: (...args: unknown[]) => void) => void;
-      removeAllListeners?: () => void;
-      isConnected?: () => boolean;
-    };
+    ethereum?: any;
   }
 }
 
